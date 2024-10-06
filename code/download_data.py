6b492f080bc898416ca289aec6829310ecbfc638
Bhanu Prasanna <bhanu.prasanna2001@gmail.com>
2024-10-06 23:52:18 +0200

    Update scripts

--- code/download_data.py
+++ code/download_data.py
@@ -2,10 +2,17 @@
 
 import pandas as pd
 import urllib.request
+import os
 
 # URL to the raw diabetes dataset (e.g., a CSV hosted online)
 url = 'https://raw.githubusercontent.com/krishnakishore999/Diabetes_Prediction/refs/heads/main/diabetes.csv'
 
+# Define the path where the data will be saved
+raw_data_path = 'data/raw/diabetes_raw.csv'
+
+# Ensure the directory exists
+os.makedirs(os.path.dirname(raw_data_path), exist_ok=True)
+
 # Download the dataset
-raw_data_path = '../data/raw/diabetes_raw.csv'
 urllib.request.urlretrieve(url, raw_data_path)
+print(f"Raw data downloaded to {raw_data_path}")
