73cebbedfc5144c6efc245e23cc944efc0780b96
Bhanu Prasanna <bhanu.prasanna2001@gmail.com>
2024-10-06 21:48:49 +0200

    Add data processing script

--- code/process_data.py
+++ code/process_data.py
@@ -5,8 +5,8 @@ import datalad.api as dl
 from sklearn.model_selection import train_test_split
 
 input_data_path = 'data/raw/diabetes_raw.csv'
-train_output_path = 'data/processed/diabetes_processed_train.csv'
-test_output_path = 'data/processed/diabetes_processed_test.csv'
+train_output_path = 'data/processed/train/diabetes_processed_train.csv'
+test_output_path = 'data/processed/test/diabetes_processed_test.csv'
 
 dl.get(input_data_path)
 
